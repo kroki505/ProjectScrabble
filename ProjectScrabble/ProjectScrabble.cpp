497709f21f1492fcd224c9b483c05eeffc036daa
kroki505 <53232227+kroki505@users.noreply.github.com>
2022-01-11 21:05:04 +0200

    Add project files.

---
+++ ProjectScrabble/ProjectScrabble.cpp
@@ -0,0 +1,22 @@
+/**
+*
+* Solution to course project # 7
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Mihail Bukov
+* @idnumber 0MI0600111
+* @compiler VC
+*
+* <file with helper functions>
+*
+*/
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+}

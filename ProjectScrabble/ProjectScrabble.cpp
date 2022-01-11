b5edd24c745be2bdbcfc4b41d20b7c9de5e00d7c
kroki505 <53232227+kroki505@users.noreply.github.com>
2022-01-11 21:43:30 +0200

    Created MainMenu function

--- ProjectScrabble/ProjectScrabble.cpp
+++ ProjectScrabble/ProjectScrabble.cpp
@@ -15,8 +15,52 @@
 
 
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+void MainMenu()
+{
+    cout << "1. Start game" << endl;
+    cout << "2. Options" << endl;
+    cout << "3. Add new word" << endl;
+    cout << "4. Exit" << endl;
+
+    int number;
+    cout << "\nChoose action: ";
+    
+    do
+    {
+        cin >> number;
+        switch (number)
+        {
+        case 1:
+        {
+
+            break;
+        }
+        case 2:
+        {
+
+            break;
+        }
+        case 3:
+        {
+
+            break;
+        }
+        case 4:
+        {
+
+            break;
+        }
+        default:
+            cout << "Invalid action! Choose again. ";
+        } 
+    } while (number < 1 || number > 4);
+    
+}
+
 int main()
 {
-    
+    MainMenu();
 }
